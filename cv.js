a060d8e5422f43d37807b42f7fde553adb4df58c
lapussy <ll.laportaa@gmail.com>
2023-02-24 13:36:21 -0300

    first commit

---
+++ cv.js
@@ -0,0 +1,13 @@
+document.getElementById('box').onclick = function mostrar(){
+    document.getElementById('columnas').style.display = 'block';
+}
+document.getElementById('box2').onclick = function mostrar(){
+    document.getElementById('columnas').style.display = 'none';
+}
+
+
+
+
+
+
+
